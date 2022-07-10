5a1cbd8fc23dd5ef19840d98478d8e0544b10e7f
Zahradeen Muazu <Zahradeenmuazu@yahoo.com>
2022-07-10 11:39:31 +0100

    done

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,19 +1,34 @@
-import { Controller, Get, Post,Body, Param } from '@nestjs/common';
+import { Controller, Get, Post,Body, Param, NotFoundException } from '@nestjs/common';
+import { createMessageDto } from './dtos/create-message.dto'
+import { MessagesService } from './messages.service'
+
 
 @Controller('messages')
 export class MessagesController {
+  messagesService : MessagesService
+
+  constructor(){
+    this.messagesService = new MessagesService()
+  }
+
   @Get()
   listMessages(){
-
+    return this.messagesService.findAll()
   }
 
   @Post()
-  createMessage(@Body() body:any){
-    console.log(body)
+  createMessage(@Body() body:createMessageDto){
+    return this.messagesService.create(body.content)
   }
 
   @Get('/:id')
-  getMessage(@Param('id') id:string){
-    console.log(id)
+  async getMessage(@Param('id') id:string){
+    const message = await this.messagesService.findOne(id)
+
+    if(!message){
+      throw new NotFoundException('message not found')
+    }
+    
+    return message
   }
 }
---
+++ src/messages/messages.service.ts
@@ -0,0 +1,18 @@
+import {MessagesRepository} from './message.repository'
+
+export class MessagesService {
+  constructor(public messagesRepo: MessagesRepository) {}
+
+ async findOne(id: string){
+  return this.messagesRepo.findOne(id);
+ }
+
+ findAll(){
+   return this.messagesRepo.findAll()
+ }
+
+ create(content:string){
+   return this.messagesRepo.create(content)
+ }
+
+}
